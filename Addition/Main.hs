dcdd2096e6ca40fd69c2f32a5dab0fc181447335
Aner Lucero <4rgento@gmail.com>
2014-10-16 13:20:06 -0300

    Add GPL3 License

--- Addition/Main.hs
+++ Addition/Main.hs
@@ -1,3 +1,21 @@
+{-
+
+Copyright (C) 2014  Aner Luero
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+-}
+
 {- Aritmetic addition couting the number of primitive operatios involved -}
 modOp :: (Int -> Int -> Int) -> Int -> Int -> Int -> Int
 modOp f base a b = mod (f a b) base
--- QueryBinarySearchTree/query_freqDist.hs
+++ QueryBinarySearchTree/query_freqDist.hs
@@ -1,3 +1,22 @@
+{- 
+
+Copyright (C) 2014  Aner Luero
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+-}
+
 {-
  - Given a file(as first CL paramter)  with lines:
  -
@@ -114,9 +133,16 @@ make_queries_from_stdin tree = do
             Nothing -> putStrLn("#" ++ query ++ "\t0") >>= \_ ->
                make_queries_from_stdin tree
 
+gpl3_notice :: String
+gpl3_notice = unlines ["QueryBinaryTree  Copyright (C) 2014 Aner Lucero",
+   "This program comes with ABSOLUTELY NO WARRANTY",
+   "This is free software, and you are welcome to redistribute it",
+   "under certain conditions"]
+
 main :: IO ()
 main = do
-   putStrLn "Welcome to (string,int) key-value search engine!"
+   putStrLn gpl3_notice
+   putStrLn "\nWelcome to (string,int) key-value search engine!"
    args <- getArgs
    putStrLn $ "Reading file: " ++ show (args !! 0)
    inTree <- read_tree_from_file (args !! 0)
